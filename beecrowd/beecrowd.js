eaf8180f5fbcb8ec5f6e412169a8eb618f6e56a4
zebra <zebra@gmail.com>
2023-03-08 12:26:05 +0200

    riba

---
+++ beecrowd/beecrowd.js
@@ -0,0 +1,10 @@
+let input = document.getElementById("input").value;
+let lines = input.split("/n");
+
+
+let a = Number(line[0]);
+let b = Number(line[1]);
+let c = Number(line[2]);
+let Otg = b*c;
+console.log("NUMBER = " + a );
+console.log("NUMBER = " + a );
